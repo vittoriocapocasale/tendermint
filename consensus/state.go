ffbf7ececac58c59ef49ab4f59e1beed7e2b37a0
Adi Seredinschi <a@seredinschi.net>
2023-02-21 15:22:34 +0100

    Shortcircuit needProofBlock

--- consensus/state.go
+++ consensus/state.go
@@ -1050,7 +1050,7 @@ func (cs *State) needProofBlock(height int64) bool {
 
 	lastBlockMeta := cs.blockStore.LoadBlockMeta(height - 1)
 	if lastBlockMeta == nil {
-		panic(fmt.Sprintf("needProofBlock: last block meta for height %d not found", height-1))
+		return true
 	}
 
 	return !bytes.Equal(cs.state.AppHash, lastBlockMeta.Header.AppHash)
